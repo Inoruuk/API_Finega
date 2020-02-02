b9ec53baa0d7722c2ec7628ef05e96fb153bc472
Alexandre Siaux <asiaux@z3r4p1.le-101.fr>
2020-02-02 16:53:15 +0100

    ajout exemple body dans route

--- static/js/routes/api.js
+++ static/js/routes/api.js
@@ -13,6 +13,7 @@ router.post('/', (req, res) => {
 
 const livemonitoringService = require('../Services/liveMonitoringService');
 
+//{"DateDebut":  "2020-01-08T00:00:00","DateFin": "2020-01-08T23:00:50","NumeroControleur": 0,"Index": 2,"Groupe": 0,"Libelle": "Position"}
 router.route('/livemonitoring/').post((req, res) => {
     livemonitoringService(req.body)
         .then((data) => {
@@ -27,6 +28,7 @@ router.route('/livemonitoring/').post((req, res) => {
 
 const productionService = require('../Services/productionService');
 
+//{"Debut": "2019-12-02T07:41:49","Fin": "2019-12-02T23:50:00"}
 router.route('/production/temps_sciage').post((req, res) => {
     productionService.tempsSciage(req.body)
         .then((data) => {
@@ -37,6 +39,7 @@ router.route('/production/temps_sciage').post((req, res) => {
         })
 });
 
+//{"Debut":"2019-12-02T00:00:00", "Fin": "2019-12-02T23:59:59", "Cycles": [[0, 25], [26, 30], [31, 35], [36, 40], [41, 50], [51, 60], [61, 90], [91, 36000]]}
 router.route('/production/cycle').post((req, res) => {
     productionService.tempsCycles(req.body)
         .then((data) => {
@@ -47,6 +50,7 @@ router.route('/production/cycle').post((req, res) => {
         })
 });
 
+//{ "DateDebut":  "2019-12-02T00:00:00", "DateFin": "2019-12-02T23:00:50", "param_diam": [[0, 240], [240, 260], [260, 280], [280, 300], [300, 320], [320, 340], [340, 360],  [360, 380], [380, 400], [400, 600]], "filtre_diam":"300,320", "filtre_longueur": 2800 }
 router.route('/production/supply').post((req, res) => {
     productionService.approvisionement(req.body)
         .then((data) => {
@@ -58,6 +62,7 @@ router.route('/production/supply').post((req, res) => {
         })
 });
 
+//{"Debut":"2019-12-02T00:00:00","Fin":"2019-12-02T23:59:59","Sections": [[18, 80], [18, 100], [20, 80], [60, 80], [80, 80]],"Filtre_longueur": 0}
 router.route('/production/production_produits').post((req, res) => {
     productionService.productionProduits(req.body)
         .then((data) => {
@@ -69,6 +74,7 @@ router.route('/production/production_produits').post((req, res) => {
         })
 });
 
+//{"Debut":"2019-12-02T00:00:00","Fin":"2019-12-02T23:59:59","Sections": [[18, 80], [18, 100], [20, 80], [60, 80], [80, 80]],"Filtre_longueur": 0}
 router.route('/production/production_volume').post((req, res) => {
     productionService.productionVolume(req.body)
         .then((data) => {
