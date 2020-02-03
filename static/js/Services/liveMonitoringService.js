0a39fab5d78175985e7e87339dd216bd7622174b
Alexandre Siaux <asiaux@z3r4p1.le-101.fr>
2020-02-03 06:26:05 +0100

    test

--- static/js/Services/liveMonitoringService.js
+++ static/js/Services/liveMonitoringService.js
@@ -5,13 +5,15 @@ function livemonitoringService(body) {
     const promise = new Promise((resolve, reject) => {
         var str = '"' + body.DateDebut + '" "' + body.DateFin + '" "' + body.NumeroControleur + '" "' + body.Index + '" "' + body.Groupe + '" "' + body.Libelle + '"';
         var string = 'python3 Script/livemonitoring.py ' + str;
-        // console.log(str, string);
         exec(string, (err, stdout, stderr) => {
         if (err) {
             console.log(stderr);
              reject({status: 500, err});
         } else {
-            const result = JSON.parse(JSON.stringify(stdout, null, 4));
+            var result = JSON.parse(JSON.stringify(stdout, null, 4));
+            result = result.split("'").join('"');
+            console.log(result);
+
             resolve({status: 200, result});
         }
         });
